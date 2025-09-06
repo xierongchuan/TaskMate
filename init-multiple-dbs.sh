417e8d7cbeb5bd2947badbe47f9c90dd05cad7af
谢榕川 <xierongchuan@tutamail.com>
2025-09-06 23:49:10 +0500

    chore(submodules): add VanillaFlowTelegramBot submodule and database
    - Added VanillaFlowTelegramBot as a new git submodule
    - Updated submodule commit for TaskMateTelegramBot
    - Created vanillaflowtelegrambot database in init-multiple-dbs.sh script

--- init-multiple-dbs.sh
+++ init-multiple-dbs.sh
@@ -4,4 +4,5 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
   CREATE DATABASE taskmatebackend;
   CREATE DATABASE taskmatetelegrambot;
+  CREATE DATABASE vanillaflowtelegrambot;
 EOSQL
