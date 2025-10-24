2ce02a7f816cb600a34437c83d362706faa26ccf
谢榕川 <xierongchuan@tutamail.com>
2025-10-25 04:28:48 +0500

    chore(env): update environment variables and database setup
    - Rename Laravel app key to task mate frontend and backend app keys
    - Split Laravel database into separate frontend and backend databases
    - Modify docker-compose example to use updated keys and database names
    - Comment adjustments for laravel frontend service in docker-compose
    - Add creation of taskmatefrontend database in initialization script
    - Update submodule commits for TaskMate components

--- init-multiple-dbs.sh
+++ init-multiple-dbs.sh
@@ -2,6 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+  CREATE DATABASE taskmatefrontend;
   CREATE DATABASE taskmatebackend;
   CREATE DATABASE taskmatetelegrambot;
   CREATE DATABASE vanillaflowtelegrambot;
